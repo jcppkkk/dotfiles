bbbd031b5e008c9f9ed71875f4bbde0a63b0e8db
Jethro Yu <comet.jc@gmail.com>
2014-03-30 04:18:38 +0800

    change comment

--- setup_dotfiles.bash
+++ setup_dotfiles.bash
@@ -4,16 +4,17 @@ current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $current
 
 
-#########
-# new machine setup
-#########
+## new machine setup
 sudo apt-get install ctags git dos2unix wget
 # sudo locale-gen zh_TW.UTF-8 || true
 
 
-#########
-# Replace dotfiles with link and backup old ones
-#########
+## install powerline
+
+pip install --user git+git://github.com/Lokaltog/powerline
+
+
+## Replace dotfiles with link and backup old ones
 dotfiles_oldfolder="$HOME/.dotfiles_old_`date +%Y%m%d%H%M%S`"
 [ ! -e "$dotfiles_oldfolder" ] && mkdir "$dotfiles_oldfolder"
 ( set +x
@@ -25,17 +26,13 @@ do
 done )
 
 
-#########
-# install vim plugins
-#########
+## install vim plugins
 [ ! -e vim/bundle/vundle ] && git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
 vim +BundleInstall! +qall
 find $HOME/.vim/ -name \*.vim -exec dos2unix -q {} \;
 
 
-#########
-# Local changes/fixes
-#########
+## Local changes/fixes
 git config branch.master.rebase true                        # Setup self default using rebase when pull
 [ "$1" = "x" ] && get fontconfig && fc-cache -vf ~/.fonts   # patch fonts for powerline
 sudo chown ${USER}. ~/.bash_history ~/.viminfo

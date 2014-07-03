80c36f8d7e35fe641e0cb9d92c743faeba9aa812
Jethro Yu <comet.jc@gmail.com>
2014-07-03 12:14:21 +0800

    Also update Vundle itself, get git-completion faster

--- update_dotfiles.bash
+++ update_dotfiles.bash
@@ -7,9 +7,14 @@ cd $current
 pip install git+git://github.com/Lokaltog/powerline --upgrade
 
 # Update vim plugins
-vim +BundleInstall! +qall
+pushd vim/bundle/vundle/
+git pull
+popd
+vim +BundleInstall +qall
 find $HOME/.vim/ -name \*.vim -exec dos2unix -q {} \;
 
 # Update git-completion.bash
-wget -N https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
-wget -N https://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
+#wget -N http://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+#wget -N http://github.com/git/git/raw/master/contrib/completion/git-prompt.sh
+wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh

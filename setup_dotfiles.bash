062e535f201119da02504ac719993cf616a1438b
Jethro Yu <comet.jc@gmail.com>
2014-11-16 19:05:10 +0800

    Compatiable to Mac OS X

--- setup_dotfiles.bash
+++ setup_dotfiles.bash
@@ -1,64 +1,109 @@
-#!/bin/bash -e
+#!/bin/bash -ex
+
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
 current="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $current
+. ./get-platform
 
 # Disable DNS resolution to speedup ssh
-sudo sed -i -e "s:^#\?UseDNS yes:UseDNS no:" /etc/ssh/sshd_config
-grep "UseDNS no" /etc/ssh/sshd_config || echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config
+if [ -f /etc/ssh/sshd_config ]; then
+    sudo sed -i -e "s:^#\?UseDNS yes:UseDNS no:" /etc/ssh/sshd_config
+    grep "UseDNS no" /etc/ssh/sshd_config || echo "UseDNS no" | sudo tee -a /etc/ssh/sshd_config
+fi
 
-## Replace dotfiles with link and backup old ones
-dotfiles_oldfolder="$HOME/.dotfiles_old_`date +%Y%m%d%H%M%S`"
-[ ! -e "$dotfiles_oldfolder" ] && mkdir "$dotfiles_oldfolder"
-( set +x
-\ls | grep -v "~$\|/setup_" | while read file;
-do 
-    target="$HOME/.$file"
-    [ -e "$target" ] && mv -f "$target" "$dotfiles_oldfolder/"
-    ln -fvs -T "$(readlink -f "$file" )" "$target"
-done )
+#######################
+## Backup dotfiles and replace with link
+#######################
 
+#dotfiles_oldfolder="$HOME/.dotfiles_old_`date +%Y%m%d%H%M%S`"
+#[ ! -e "$dotfiles_oldfolder" ] && mkdir "$dotfiles_oldfolder"
+#( 
+#\ls | grep -v "~$\|/setup_" | while read file;
+#do 
+#    target="$HOME/.$file"
+#    [ -e "$target" ] && mv -f "$target" "$dotfiles_oldfolder/"
+#    ln -fvs "$(realpath "$file" )" "$target"
+#done )
 
-## new machine setup
-packages="exuberant-ctags git dos2unix wget make build-essential libssl-dev zlib1g-dev libbz2-dev \
-	libreadline-dev libsqlite3-dev wget curl llvm"
+#######################
+## install packages on new machine
+#######################
+
+packages="git dos2unix wget wget curl"
+
+case $platform in 
+    'linux') 
+        packages="$packages exuberant-ctags"
+        packages="$packages make"
+        packages="$packages build-essential"
+        packages="$packages libssl-dev"
+        packages="$packages libbz2-dev"
+        packages="$packages zlib1g-dev"
+        packages="$packages libreadline-dev"
+        packages="$packages libsqlite3-dev"
+        ;;
+    'mac')
+        brew help > /dev/null || ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        packages="$packages ctags"
+        packages="$packages python"
+        ;;
+esac
+
+linux_check_pkg() { pkg -s "$1" >/dev/null 2>&1; }
+linux_install_pkg() { sudo apt-get install -y $@; }
+mac_check_pkg() { brew list -1 | grep -q "^${1}\$"; }
+mac_install_pkg() { brew install $@; }
+
 install_packages=""
 for P in $packages; do
-	if dpkg -s "$P" >/dev/null 2>&1; then 
-		echo "$P is installed."
-	else
-		echo "$P is not installed."
-		install_packages="$install_packages $P"
-	fi
+    if ${platform}_check_pkg $P; then 
+        echo "$P is installed."
+    else
+        echo "$P is not installed."
+        install_packages="$install_packages $P"
+    fi
 done
-[ -n "$install_packages" ] && sudo apt-get install -y $install_packages
+[ -n "$install_packages" ] && ${platform}_install_pkg $install_packages
 
 # sudo locale-gen zh_TW.UTF-8 || true
 
 
-## install pyenv & powerline
-CFLAGS='-g -O2'
-curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
-export setupdotfile=yes
-set +e
-source ~/.bashrc
-set -e
-pyenv versions | grep -q 2.7.7 || pyenv install 2.7.7
-pyenv global 2.7.7
-pip install git+git://github.com/Lokaltog/powerline
-
-
+#######################
 ## install vim plugins
+#######################
 [ -e vim/bundle/vundle ] && (cd vim/bundle/vundle; git pull)
 [ ! -e vim/bundle/vundle ] && git clone https://github.com/gmarik/vundle.git vim/bundle/vundle
 vim +BundleInstall +qall
 find $HOME/.vim/ -name \*.vim -exec dos2unix -q {} \;
+
+#######################
+## install pyenv
+#######################
+#CFLAGS='-g -O2'
+#curl -L https://raw.github.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash
+#export setupdotfile=yes
+#set +e
+#source ~/.bashrc
+#set -e
+#pyenv versions | grep -q 2.7.7 || pyenv install 2.7.7
+#pyenv global 2.7.7
+
+#######################
+## install powerline
+#######################
+[[ $platform == 'mac' ]] && pip install git+git://github.com/Lokaltog/powerline
+[[ $platform == 'linux' ]] && pip install --user git+git://github.com/Lokaltog/powerline
 
 
 ## Local changes/fixes
 rm -rf local
 git config branch.master.rebase true                        # Setup self default using rebase when pull
 [ "$1" = "x" ] && get fontconfig && fc-cache -vf ~/.fonts   # patch fonts for powerline
-[ -e ~/.bash_history ] && sudo -s chown ${USER}. ~/.bash_history
-[ -e ~/.viminfo ] && sudo -s chown ${USER}. ~/.viminfo
+gid=`id -g`
+[ -e ~/.bash_history ] && sudo -k -s chown ${USER}:${gid} ~/.bash_history
+[ -e ~/.viminfo ] && sudo -k -s chown ${USER}:${gid} ~/.viminfo
 
 exec bash -i # reload bash

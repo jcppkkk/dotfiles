c13f3421ead590209def3668e5a24355f3e7277f
Jethro Yu <comet.jc@gmail.com>
2015-04-07 11:05:44 +0800

    update vim in git cronjob

--- git_update_dotfiles.bash
+++ git_update_dotfiles.bash
@@ -2,4 +2,9 @@
 hash git || exit
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-git remote update > /dev/null
+git fetch --all --prune > /dev/null
+
+pushd vim
+git fetch --all --prune > /dev/null
+vim +BundleInstall +qa
+popd

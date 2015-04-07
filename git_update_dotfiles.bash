aee09b0039bea9ec5259947403587b518c9106ca
Jethro Yu <comet.jc@gmail.com>
2015-04-07 11:18:55 +0800

    supress auto update outputs

--- git_update_dotfiles.bash
+++ git_update_dotfiles.bash
@@ -1,10 +1,11 @@
 #!/bin/bash
+exec > /dev/null
 hash git || exit
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
-git fetch --all --prune > /dev/null
+git fetch --all --prune 
 
 pushd vim
-git fetch --all --prune > /dev/null
+git pull 
 vim +BundleInstall +qa
 popd

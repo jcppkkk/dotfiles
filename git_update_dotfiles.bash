8f53b81e1746ad73ac2ed50049a327b8b08e3bee
Jethro Yu <comet.jc@gmail.com>
2015-08-19 10:42:40 +0800

    update update script

--- git_update_dotfiles.bash
+++ git_update_dotfiles.bash
@@ -1,11 +1,10 @@
 #!/bin/bash
 exec > /dev/null
 hash git || exit
+hash vim || exit
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 git fetch --all --prune 
+git pull --rebase
 
-pushd vim
-git pull 
 vim +BundleInstall +qa
-popd

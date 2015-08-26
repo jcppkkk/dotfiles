73f52fdbe3b45edc8c0f8d6fea2f92725fc31735
Jethro Yu <comet.jc@gmail.com>
2015-08-26 14:54:46 +0800

    upgrade powerline-status

--- git_update_dotfiles.bash
+++ git_update_dotfiles.bash
@@ -7,5 +7,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 git fetch --all --prune
 git pull --rebase
 
+hash powerline-daemon && powerline-daemon -k || true
+if (which powerline | grep /usr -q); then
+	sudo -H pip install powerline-status --upgrade
+else
+	pip install --user powerline-status --upgrade
+fi
+
 (cd ~/dotfiles/vim; mv -f vundle Vundle.vim)
 vim +BundleInstall +qa

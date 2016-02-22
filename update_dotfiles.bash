87d509913dccb59c8d922f0f79767c12c632554a
Jethro Yu <comet.jc@gmail.com>
2016-02-23 02:33:41 +0800

    update scripts

--- update_dotfiles.bash
+++ update_dotfiles.bash
@@ -6,13 +6,9 @@ cd $current
 # Clean old files
 [ -d local ] && echo The local folder for powerline is not needed anymore, remove? && \rm -ri local
 
-# Update powerline
+# Update python pkgs
 hash powerline-daemon && powerline-daemon -k || true
-if (which powerline | grep /usr -q); then
-	sudo pip install powerline-status powerline-gitstatus argparse --upgrade
-else
-	pip install --user powerline-status powerline-gitstatus argparse --upgrade
-fi
+sudo -H pip install --upgrade -r requirements_dotfiles.txt
 
 # Update vim plugins
 if [ -e vim/bundle/vundle ]; then
@@ -25,7 +21,7 @@ find $HOME/.vim/ -name \*.vim -exec dos2unix -q {} \;
 
 # Update git-completion.bash
 wget -N https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
-pushd bash_completion.d/
+pushd bashrc.d/
 wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 wget -N https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash

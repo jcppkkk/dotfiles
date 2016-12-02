e6396c209c481dbf644f58795816430c9362755a
Jethro Yu <comet.jc@gmail.com>
2016-12-02 18:56:48 +0800

    more manpages

--- setup_dotfiles.bash
+++ setup_dotfiles.bash
@@ -98,6 +98,7 @@ case $platform in
 	packages+=(exuberant-ctags silversearcher-ag) # Coding tools
 	packages+=(meld tig) # SVC tools
 	packages+=(unzip)
+	packages+=(manpages-dev manpages-posix-dev manpages-zh)
 	if lsb_release -a | grep 14.04; then
 		packages+=(vim)
 	else

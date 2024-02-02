abef50625e09dfc60a46505b9f2aac6eaf430e97
Jethro Yu <comet.jc@gmail.com>
2024-02-02 10:44:03 +0800

    fix(update_dotfiles): fix directory path for bashrc.d

--- update_dotfiles.bash
+++ update_dotfiles.bash
@@ -24,7 +24,7 @@ find "$HOME/.vim/" -name \*.vim -exec dos2unix -q {} \;
 
 # Update git-completion.bash
 wget -N https://raw.githubusercontent.com/seebi/dircolors-solarized/master/dircolors.256dark
-pushd bashrc.d/
+pushd files/.bashrc.d/
 wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 wget -N https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 wget -N https://raw.githubusercontent.com/bobthecow/git-flow-completion/master/git-flow-completion.bash
